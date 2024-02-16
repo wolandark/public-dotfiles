b32e6a0d129fb76f7f595bdc3a93d21e08f98bd5
wolandark <isitjuicyisitfresh@tutanota.com>
2024-02-17 01:41:11 +0330

    UP

---
+++ local-bin/keyboardrate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+xmodmap -e "keycode 135 = Super_L"
+#xmodmap -e 'clear Lock' -e 'keycode 66 = Escape'
+#xmodmap -e 'clear Lock' -e 'keycode 9 = Caps_Lock'
+# xset r rate 250 40
+xset r rate 270 60
